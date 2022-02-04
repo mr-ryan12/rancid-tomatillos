9ea5c0a5dde879f999a79c74a92b2d80043086d1
Ryan McBride <mr.ryan444@gmail.com>
2022-02-03 20:36:43 -0700

    Remove commented out code

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,10 +4,10 @@ import '../styles/MovieCard.scss'
 const MovieCard = props => {
   const splitDate = props.releaseDate.split('-')
   let [year, month, day] = splitDate
-  
+
   return (
     <section className="movie-card" tabIndex={0}>
-      <img src={props.posterImg} alt={`Movie Poster for ${props.title}`}/>
+      <img src={props.posterImg} alt={`Movie Poster for ${props.title}`} onClick={() => console.log('Hello!')}/>
       <h2>{props.title}</h2>
       <h3>Release Date: {[month, day, year].join('-')}</h3>
       <h3>Rating: {props.averageRating.toFixed(2)}</h3>
