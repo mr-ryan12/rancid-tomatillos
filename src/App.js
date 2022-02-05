ef5b0177dcb47fe662e2100286ad3d22e02a4a31
Ryan McBride <mr.ryan444@gmail.com>
2022-02-04 18:56:01 -0700

    Change data to fetch calls

--- src/App.js
+++ src/App.js
@@ -8,31 +8,44 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      movies: movieData,
+      movies: [],
       isSingleMovie: false,
       movie: ''
     }
   }
 
+  componentDidMount = () => {
+    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ movies: data.movies })
+      })
+  }
+
   displayIndividualMovie = id => {
     if (id) {
-      this.setState({
-        isSingleMovie: true,
-        movie: {
-          id: 1,
-          title: "Fake Movie Title",
-          poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg",
-          backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg",
-          release_date: "2019-12-04",
-          overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!",
-          average_rating: 6,
-          genres: ["Drama", "other things"],
-          budget:63000000,
-          revenue:100853753,
-          runtime:139,
-          tagline: "It's a movie!"
-        }
-      })
+      // this.setState({
+      //   isSingleMovie: true,
+      //   movie: {
+      //     id: 1,
+      //     title: "Fake Movie Title",
+      //     poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg",
+      //     backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg",
+      //     release_date: "2019-12-04",
+      //     overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!",
+      //     average_rating: 6,
+      //     genres: ["Drama", "other things"],
+      //     budget:63000000,
+      //     revenue:100853753,
+      //     runtime:139,
+      //     tagline: "It's a movie!"
+      //   }
+      // })
+      fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+        .then(response => response.json())
+        .then(data => {
+          this.setState({ isSingleMovie: true, movie: data.movie })
+        })
     }
   }
 
