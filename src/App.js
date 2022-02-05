07df83f9423aacda425bb91775c5066f5f94f82a
Ryan McBride <mr.ryan444@gmail.com>
2022-02-04 20:22:36 -0700

    Add model message for error handling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Movies from './components/Movies'
 import movieData from './moviesData'
 import IndividualMovie from './components/IndividualMovie'
+import ErrorModal from './components/ErrorModal'
 import './styles/App.scss';
 
 class App extends Component {
@@ -10,24 +11,43 @@ class App extends Component {
     this.state = {
       movies: [],
       isSingleMovie: false,
-      movie: ''
+      movie: '',
+      error: ''
     }
   }
 
   componentDidMount = () => {
-    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-      .then(response => response.json())
+    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies1')
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error('Something went wrong. Please refresh the page or try again later.')
+        }
+      })
       .then(data => {
         this.setState({ movies: data.movies })
+      })
+      .catch(error => {
+        this.setState({ error: error.message })
       })
   }
 
   displayIndividualMovie = id => {
     if (id) {
       fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
-        .then(response => response.json())
+        .then(response => {
+          if (response.ok) {
+            return response.json()
+          } else {
+            throw new Error('Something went wrong. Please refresh the page or try again later.')
+          }
+        })
         .then(data => {
           this.setState({ isSingleMovie: true, movie: data.movie })
+        })
+        .catch(error => {
+          this.setState({ error: error.message })
         })
     }
   }
@@ -41,6 +61,7 @@ class App extends Component {
       <main className="main-container">
         <h1>Rotten Tomatillos</h1>
         <h2 className="opening-line">Where your imagination comes to life on the big screen</h2>
+        {this.state.error ? <ErrorModal message={this.state.error}/> : null}
         {!this.state.isSingleMovie ? <Movies movies={this.state.movies} findMovie={this.displayIndividualMovie} /> : <IndividualMovie movie={this.state.movie} displayHomePage={this.displayHomePage}/>}
       </main>
     )
