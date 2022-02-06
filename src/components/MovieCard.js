bc5585339be8214d6052404b2d9aea8f98e0ee8a
Christine Rowland <fordo29@gmail.com>
2022-02-06 16:47:22 -0700

    Move conditional rendering outside of the return statement

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,12 +4,13 @@ import '../styles/MovieCard.scss'
 const MovieCard = props => {
   const splitDate = props.releaseDate.split('-')
   let [year, month, day] = splitDate
+  const releaseDateFormat2 = [month, day, year].join('-')
 
   return (
     <section className="movie-card" tabIndex={0}>
       <img src={props.posterImg} alt={`Movie Poster for ${props.title}`} onClick={() => props.findMovie(props.id)}/>
       <h2>{props.title}</h2>
-      <h3><span className='bolder'>Release Date: </span>{[month, day, year].join('-')}</h3>
+      <h3><span className='bolder'>Release Date: </span>{releaseDateFormat2}</h3>
       <h3><span className='bolder'>Rating: </span>{props.averageRating.toFixed(2)}</h3>
     </section>
   )
