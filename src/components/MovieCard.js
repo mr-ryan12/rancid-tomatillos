14777a2856527f2993b78c3088eacc442a72afbc
Christine Rowland <fordo29@gmail.com>
2022-02-06 12:28:03 -0700

    Update styling on MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -9,8 +9,8 @@ const MovieCard = props => {
     <section className="movie-card" tabIndex={0}>
       <img src={props.posterImg} alt={`Movie Poster for ${props.title}`} onClick={() => props.findMovie(props.id)}/>
       <h2>{props.title}</h2>
-      <h3>Release Date: {[month, day, year].join('-')}</h3>
-      <h3>Rating: {props.averageRating.toFixed(2)}</h3>
+      <h3><span className='bold'>Release Date: </span>{[month, day, year].join('-')}</h3>
+      <h3><span className='bold'>Rating: </span>{props.averageRating.toFixed(2)}</h3>
     </section>
   )
 }
