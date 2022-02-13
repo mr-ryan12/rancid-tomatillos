0f0aae22026104820616538a38113ee8530430d4
Christine Rowland <fordo29@gmail.com>
2022-02-12 18:53:10 -0700

    Save 2 working tests

--- cypress/integration/trailerPage_spec.js
+++ cypress/integration/trailerPage_spec.js
@@ -1,11 +1,30 @@
 describe('Trailer Page User Flow', () => {
   beforeEach(() => {
-    cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}/videos', { fixture: 'individualHappyMovie.json' })
+    cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/726739/videos', { fixture: 'trailerHappyPath.json' })
     cy.visit('http://localhost:3000')
       .contains('.movie-card', 'Cats & Dogs')
       .find('img')
       .click()
+      .get('.button-container')
+      .find('.trailer-link')
+      .click().wait(45)
+ 
   });
 
+  it('Should be able to visit the trailer page and the user will see the application name & fun tagline', () => {
+    cy.contains('Rotten Tomatillos')
+      .get('.text-focus-in')
+      .contains('Where your imagination comes to life on the big screen')
+  });
 
+  it('should display all working trailers available for the movie', () => {
+    cy.get('.video')
+    .should('have.length', 11)
+  })
+
+   it('should display 2 buttons at the bottom of the page', () => {
+    cy.get('.trailer-container').find('.back-home-button').contains('Home')
+    cy.get('.trailer-container').find('.back-to-movie-button').contains('Back to Movie Details')
+  })
+  
 });
